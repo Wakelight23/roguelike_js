ed0967ae9c4b1bd7e644d1ab5d51659ba2cc35ab
wakelight23 <rlghks0223@gmail.com>
2025-04-23 17:50:15 +0900

    Update : start.bat
    - 최초 1회 실행 시 npm install 이 실행되도록 수정

--- start.bat
+++ start.bat
@@ -1,3 +1,11 @@
 @echo off
 chcp 65001
+
+if not exist "node_modules" (
+    echo node_modules not found. Running npm install...
+    call npm install
+) else (
+    echo node_modules exists. Skipping npm install.
+)
+
 npm start
